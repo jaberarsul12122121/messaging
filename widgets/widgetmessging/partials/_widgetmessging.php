b4276c16fefa2c9e0c2bb8df68d29358edc0c661
hiwa <hywabas6@acornassociated.org>
2023-05-18 10:38:38 +0300

    i modified Messaging.css file and Widgetmessging.php

--- widgets/widgetmessging/partials/_widgetmessging.php
+++ widgets/widgetmessging/partials/_widgetmessging.php
@@ -5,17 +5,158 @@
                 <i class="icon-wechat"></i>
             </div>
             <div class="searchSidepanl">
-                <input type="input" class="search" placeholder="search.....">
+                <input type="input" class="search" placeholder="search....." >
+                <i class="fa-solid fa-magnifying-glass"></i>
             </div>
         </div>
     </div>
     <div class="towDiv">
         <button class="buttonMessage">
-            <p> <i class="oc-icon-plus"></i>Message</p>
+            <p><i class="oc-icon-plus"></i>Message</p>
         </button>
         <button class="iconbutton">
             <i class="wn-icon-sort-alpha-asc"></i>
         </button>
     </div>
-    <div class="threeDiv"></div>
+    <div class="threeDiv">
+        <div>
+            <button>User</button>
+        </div>
+        <div>
+            <button>Group</button>
+        </div>
+    </div>
+    <div class="fourDiv">
+        <ul >
+            <li class="one-person">
+                <div class="img-ind">
+                   <img  src="" alt="">
+                   <div class="ind"></div>
+                </div>
+                <div class="text">
+                    <span>jaber</span>
+                 <pre>dolor unde consectetur ut neque. Necessitatibus</pre>
+                </div>
+                <div class="time">
+                    <span>2:00 am</span>
+                    <div class="blue-circle">
+                        <div class="number">3</div>
+                    </div>
+                </div>
+            </li>
+            <li class="one-person">
+                <div class="img-ind">
+                   <img  src="" alt="">
+                   <div class="ind"></div>
+                </div>
+                <div class="text">
+                    <span>jaber</span>
+                 <pre>dolor unde consectetur ut neque. Necessitatibus</pre>
+                </div>
+                <div class="time">
+                    <span>2:00 am</span>
+                    <div class="blue-circle">
+                        <div class="number">3</div>
+                    </div>
+                </div>
+            </li>
+            <li class="one-person">
+                <div class="img-ind">
+                   <img  src="" alt="">
+                   <div class="ind"></div>
+                </div>
+                <div class="text">
+                    <span>jaber</span>
+                 <pre>dolor unde consectetur ut neque. Necessitatibus</pre>
+                </div>
+                <div class="time">
+                    <span>2:00 am</span>
+                    <div class="blue-circle">
+                        <div class="number">3</div>
+                    </div>
+                </div>
+            </li>
+            <li class="one-person">
+                <div class="img-ind">
+                   <img  src="" alt="">
+                   <div class="ind"></div>
+                </div>
+                <div class="text">
+                    <span>jaber</span>
+                 <pre>dolor unde consectetur ut neque. Necessitatibus</pre>
+                </div>
+                <div class="time">
+                    <span>2:00 am</span>
+                    <div class="blue-circle">
+                        <div class="number">3</div>
+                    </div>
+                </div>
+            </li>
+            <li class="one-person">
+                <div class="img-ind">
+                   <img  src="" alt="">
+                   <div class="ind"></div>
+                </div>
+                <div class="text">
+                    <span>jaber</span>
+                 <pre>dolor unde consectetur ut neque. Necessitatibus</pre>
+                </div>
+                <div class="time">
+                    <span>2:00 am</span>
+                    <div class="blue-circle">
+                        <div class="number">3</div>
+                    </div>
+                </div>
+            </li>
+            <li class="one-person">
+                <div class="img-ind">
+                   <img  src="" alt="">
+                   <div class="ind"></div>
+                </div>
+                <div class="text">
+                    <span>jaber</span>
+                 <pre>dolor unde consectetur ut neque. Necessitatibus</pre>
+                </div>
+                <div class="time">
+                    <span>2:00 am</span>
+                    <div class="blue-circle">
+                        <div class="number">3</div>
+                    </div>
+                </div>
+            </li>
+            <li class="one-person">
+                <div class="img-ind">
+                   <img  src="" alt="">
+                   <div class="ind"></div>
+                </div>
+                <div class="text">
+                    <span>jaber</span>
+                 <pre>dolor unde consectetur ut neque. Necessitatibus</pre>
+                </div>
+                <div class="time">
+                    <span>2:00 am</span>
+                    <div class="blue-circle">
+                        <div class="number">3</div>
+                    </div>
+                </div>
+            </li>
+            <li class="one-person">
+                <div class="img-ind">
+                   <img  src="" alt="">
+                   <div class="ind"></div>
+                </div>
+                <div class="text">
+                    <span>jaber</span>
+                 <pre>dolor unde consectetur ut neque. Necessitatibus</pre>
+                </div>
+                <div class="time">
+                    <span>2:00 am</span>
+                    <div class="blue-circle">
+                        <div class="number">3</div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="fiveDiv"></div>
 </div>
