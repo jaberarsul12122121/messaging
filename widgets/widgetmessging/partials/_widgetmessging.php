c8a2907159b0e46ca844f31f35e3d89b1b4d8b83
hiwa <hywabas6@acornassociated.org>
2023-05-20 14:28:54 +0300

    modified messaging.css and widgetmessaging.php

--- widgets/widgetmessging/partials/_widgetmessging.php
+++ widgets/widgetmessging/partials/_widgetmessging.php
@@ -2,7 +2,7 @@
     <div class="oneDiv">
     <?= $this->makePartial('header') ?>
     </div>
-    <div class="towDiv">
+    <!-- <div class="towDiv">
         <button class="buttonMessage">
             <p><i class="oc-icon-plus"></i>Message</p>
         </button>
@@ -12,9 +12,26 @@
     </div>
     <div class="threeDiv">
         <?= $this->makePartial('usergruop') ?>
+    </div> -->
+    <div class="all">
+        <div>
+            <button class="buttonMessage">
+            <p><i class="oc-icon-plus"></i>Message</p>
+            </button>
+        </div>
+        <div class="icon">
+           <button class="iconbutton">
+               <i class="wn-icon-sort-alpha-asc"></i>
+           </button>  
+        </div>
+        <div class="flex">
+        <button>User</button>
+        <button>Group</button>
+        </div>
+        
     </div>
     <div class="fourDiv">
         <?= $this->makePartial('listuser') ?>
     </div>
-    <div class="fiveDiv"></div>
+  
 </div>
